d1c0efbb27747c0141da2dad740a8b1849d46417
Vadim Polyakov <vadim@preparedmind.net>
2017-06-04 16:28:45 -0400

    cleanup .vagrant dir

--- 01-docker/Vagrant-build/prebuilt/10-run-app.sh
+++ 01-docker/Vagrant-build/prebuilt/10-run-app.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-export MONGO_CONNECTION_STR=mongodb://VadimMongoDB2017:MongoDB2017@ds155961.mlab.com:55961/mongodb_2017
+export MONGO_CONNECTION_STR=mongodb://<username>:<password>@<server>:<port>/mongodb_2017
 cd ~/demoapp/app/server
 node ./index.js &
